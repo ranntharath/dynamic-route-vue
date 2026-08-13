f5b99bd59c92719c453095a2a6e20f9134728f42
ranntharath <tharath8061@gmail.com>
2026-08-13 11:05:30 +0700

    loading

--- src/pages/ProductDetail.vue
+++ src/pages/ProductDetail.vue
@@ -11,36 +11,53 @@ type Product = {
   image: string;
 };
 
+
 const route = useRoute();
 const productId = Number(route.params.id);
 
 const product = ref<Product | null>(null);
+const isLoading = ref<boolean>(false);
 
 async function getProductById() {
-  try{
+  isLoading.value = true;
+  try {
     const res = await fetch(`http://localhost:3000/products/${productId}`);
     const data = await res.json();
+    if(data?.error){
+      product.value = null;
+      return;
+    }
     product.value = data;
-  }catch(error){
+  } catch (error) {
     console.log(error);
+    
+  } finally {
+    isLoading.value = false;
   }
 }
 
-onMounted(()=>{
+onMounted(() => {
   getProductById();
-})
-
+});
 </script>
 
 <template>
-  <div v-if="!product">
-    <h1 class="text-3xl text-center">Product not found</h1>
+
+  <div v-if="isLoading">
+    <h1 class="text-3xl text-center">Loading...</h1>
   </div>
+
   <div v-else>
-    <h1 class="text-2xl font-bold">Product Detail Page</h1>
-    <img :src="product?.image" alt="" />
-    <p>{{ product?.name }}</p>
-    <p>{{ product?.description }}</p>
-    <p>${{ product?.price }}</p>
+    
+    <div v-if="!product">
+      <h1 class="text-3xl text-center">Product not found</h1>
+    </div>
+    <div v-else>
+      <h1 class="text-2xl font-bold">Product Detail Page</h1>
+      <img :src="product?.image" alt="" />
+      <p>{{ product?.name }}</p>
+      <p>{{ product?.description }}</p>
+      <p>${{ product?.price }}</p>
+    </div>
   </div>
 </template>
